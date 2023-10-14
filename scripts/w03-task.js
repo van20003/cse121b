a61f0427772ca733c5f89caaf985031512e0cbf8
yourusername <van2003@byui.edu>
2023-10-14 17:13:54 -0600

    commit

---
+++ scripts/w03-task.js
@@ -0,0 +1,44 @@
+/* LESSON 3 - Programming Tasks */
+
+/* FUNCTIONS */
+/* Function Definition - Add Numbers */
+function add (number1, number2) {
+    return number1 + number2;
+    // function body
+}
+
+let addNumber1 = Number(document.querySelector("#add1").value);
+let addNumber2 = Number(document.querySelector("#add2").value);
+
+function addNumbers() {
+let addNumber1 = Number(document.querySelector("#add1").value);
+let addNumber2 = Number(document.querySelector("#add2").value);
+let sum = add(addNumber + addNumber2);
+document.querySelector("#sum").value = sum;
+}
+
+
+/* Function Expression - Subtract Numbers */
+
+
+/* Arrow Function - Multiply Numbers */
+
+
+/* Open Function Use - Divide Numbers */
+
+
+/* Decision Structure */
+
+
+/* ARRAY METHODS - Functional Programming */
+/* Output Source Array */
+
+/* Output Odds Only Array */
+
+/* Output Evens Only Array */
+
+/* Output Sum of Org. Array */
+
+/* Output Multiplied by 2 Array */
+
+/* Output Sum of Multiplied by 2 Array */
