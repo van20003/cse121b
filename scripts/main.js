77468aee8882a6f619fdf7edefecf84cea4d6a83
yourusername <van2003@byui.edu>
2023-10-24 18:33:01 -0600

    commit

--- scripts/main.js
+++ scripts/main.js
@@ -8,3 +8,26 @@ const toggleMenu = () => {
 
 document.querySelector('#toggleMenu').addEventListener('click', toggleMenu);
 
+
+// Import helper 
+import { calculateTip, updateTipDisplay } from './tipCalculator.js';
+
+// DOM elements
+const billInput = document.getElementById("bill");
+const tipSelect = document.getElementById("tipAmount");
+const calculateButton = document.getElementById("calculateButton");
+
+// Attach event to button
+calculateButton.addEventListener('click', () => {
+
+
+// Get values of Total Cost and Tip Percent
+    const totalBill = parseFloat(billInput.value);
+    const tipAmount = parseFloat(tipSelect.value);
+
+// Calculate tip
+    const total = calculateTip(totalBill, tipAmount);
+
+// Display tip
+    updateTipDisplay(total);
+});
