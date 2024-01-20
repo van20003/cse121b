d6156af62fb3e83afc2b115b2c06018e2746b801
yourusername <van2003@byui.edu>
2024-01-20 11:13:34 -0700

    commit

---
+++ scripts/w02-task.js
@@ -0,0 +1,28 @@
+/* W02-Task - Profile Home Page */
+
+/* Step 1 - Setup type tasks - no code required */
+
+/* Step 2 - Variables */
+let fullName = "James Vang";
+let currentYear = "2023";
+let profilePicture = 'images/Me.jpg';
+let favfoods = ["Steak","Burgers", "Ramen"];
+
+
+/* Step 3 - Element Variables */
+const nameElement = document/getElementById("name");
+const foodElement = document.getElementById("food");
+const yearElement = document.querySelector("#year");
+
+
+/* Step 4 - Adding Content */
+
+nameElement.innerHTML = "<strong>${fullName}</strong>";
+yearElement.textContent = currentYear;
+
+
+
+
+/* Step 5 - Array */
+
+
